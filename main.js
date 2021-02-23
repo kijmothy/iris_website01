b11c8d504686a3bd4d54ebb3cfba11ac3f4548ec
kijmothy <76633798+kijmothy@users.noreply.github.com>
2021-02-23 18:56:17 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function show(){
+    let mobileMenu=document.querySelector("#mobile-menu");
+    mobileMenu.style.display="block";
+}
+function none(){
+    let mobileMenu=document.querySelector("#mobile-menu");
+    mobileMenu.style.display="none"
+}
